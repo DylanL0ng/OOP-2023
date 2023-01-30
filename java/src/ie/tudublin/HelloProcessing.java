332f442dff70f83e45b104f23d381f132a6bc948
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-01-30 12:20:24 +0000

    Basic Syntax

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -7,13 +7,22 @@ public class HelloProcessing extends PApplet
 
 	public void settings()
 	{
-		size(500, 500);
+		size(400, 500);
 	}
 
 	public void setup() {
+		colorMode(RGB);
+		background(0);
 	}
 
 	public void draw()
-	{	
+	{
+		stroke(255);
+		line(10, 10, 100, 100);
+		circle(100, 250, 70);
+		rect(10, 300, 20, 100);
+		stroke(255, 255, 255);
+		triangle(10, 80, 50, 80, 100, 180);
+		// triangle(40, 90, 300, 400, 900, 1000);
 	}
 }
