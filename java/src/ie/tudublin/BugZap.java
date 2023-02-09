ba4ab562b4f5ca029f3833dacfa933271b8dc074
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-02-09 01:24:02 +0000

    Initial BugZap commit

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,63 +5,105 @@ import processing.core.PApplet;
 public class BugZap extends PApplet
 {
 
+
+
 	public void settings()
 	{
 		size(500, 500);
 	}
 
 	public void setup() {
-		colorMode(HSB);
-		background(0);
-
-		x1 = random(0, width);
-		x2 = random(0, width);
-		y1 = random(0, height);
-		y2 = random(0, height);
-
-		float range = 5;
-
-		x1dir = random(-range, range);
-		x2dir = random(-range, range);
-		y1dir = random(-range, range);
-		y2dir = random(-range, range);
-
-		smooth();
+		background(0, 0, 0);
 		
-	}
+		playerX = width / 2.0f;
+		playerY = height / 2.0f;
+		playerWidth = 40.0f;
+		playerSpeed = 5.0f;
+		playerHeight = 0.0f;
+		playerBounds = (playerWidth / 2);
 
-	float x1, y1, x2, y2;
-	float x1dir, x2dir, y1dir, y2dir;
-	float c = 0;
+		bugWidth = 10.0f;
+	}
+	
+	float playerX, playerY, playerWidth, playerHeight, playerBounds, playerSpeed, bugWidth = 0.0f;
 	
 	public void draw()
 	{	
-		strokeWeight(2);
-		stroke(c, 255, 255);
-		c = (c + 1f) % 255;
-		line(x1, y1, x2, y2);
+		background(0, 0, 0);
+		drawPlayer(playerX, playerY, playerWidth, playerHeight);
+		drawBug(100, 100);
+		smooth();
+	}
 
-		x1 += x1dir;
-		x2 += x2dir;
-		y1 += y1dir;
-		y2 += y2dir;
+	public void keyPressed()
+	{
+		// Y is inverted since it starts from top left
+		if (keyCode == LEFT || key == 'a')
+		{
+			playerX -= playerSpeed;
+			if (
+				(playerX - playerBounds > width) ||
+				(playerX - playerBounds < 0)
+			)
+				playerX += playerSpeed;
+		}
+		else if (keyCode == RIGHT || key == 'd')
+		{
+			playerX += playerSpeed;
+			if ((playerX + playerBounds) > width || (playerX + playerBounds) < 0)
+				playerX -= playerSpeed;
+		}
 		
-		if (x1 < 0 || x1 > width)
+		if (keyCode == UP || key == 'w')
 		{
-			x1dir = - x1dir;
+			playerY -= playerSpeed;
+			if ((playerY - playerBounds > height) || (playerY - playerBounds < 0) )
+				playerY += playerSpeed;
 		}
-		if (y1 < 0 || y1 > height)
+		else if (keyCode == DOWN || key == 's')
 		{
-			y1dir = - y1dir;
+			playerY += playerSpeed;
+			if ((playerY + playerBounds > height) || (playerY + playerBounds < 0))
+				playerY -= playerSpeed;
 		}
-
-		if (x2 < 0 || x2 > width)
+	
+		if (key == ' ')
 		{
-			x2dir = - x2dir;
-		}
-		if (y2 < 0 || y2 > height)
-		{
-			y2dir = - y2dir;
+			stroke(255, 0, 0);
+			strokeWeight(1.0f);
+			line(playerX, playerY, mouseX, mouseY);
 		}
 	}
+
+	private void drawBug(float x, float y)
+	{
+		stroke(0, 255, 0);
+		strokeWeight(bugWidth);
+		line(x, y, x, y);
+	}
+
+	private void drawPlayer(float x, float y, float w, float h)
+	{
+		stroke(255, 255, 255);
+		strokeCap(ROUND	);
+		strokeWeight(w);
+		line(x, y, x, y);
+
+		strokeWeight(w/2);
+		drawCannon(x, y, mouseX, mouseY, 35f);
+	}
+
+	private void drawCannon(float x1, float y1, float x2, float y2, float maxLen) {
+		float lineLen = (float) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		if (lineLen > maxLen) {
+			float ratio = maxLen / lineLen;
+			x2 = x1 + (x2 - x1) * ratio;
+			y2 = y1 + (y2 - y1) * ratio;
+		}
+
+		strokeCap(SQUARE);
+		line(x1, y1, x2, y2);
+	}
+	
+
 }
