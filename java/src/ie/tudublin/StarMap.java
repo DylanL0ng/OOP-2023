8f52c52339487ce54e85a5ee2e0dbf91bceabe54
Opod <48951218+DylanL0ng@users.noreply.github.com>
2023-02-24 15:25:00 +0000

    starmap

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -20,6 +20,24 @@ public class StarMap extends PApplet
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);	
+		drawGrid();	
+	}
+
+	public void drawGrid()
+	{
+		stroke(100, 300, 100);
+		noFill();
+
+		int count = 10;
+		float border = 50.0f;
+		float gap = (width - (border * 2.0f)) /  (float) count;
+
+		for (int i = -5; i <= 5; i++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+		}
 	}
 }
